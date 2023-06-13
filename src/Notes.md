2be9495871b8e404cb62234db26f0f86b8ece18b
Pritam <pritam.saini@indusnet.co.in>
2023-06-13 19:14:03 +0530

    first commit on Angular calculator

---
+++ src/Notes.md
@@ -0,0 +1,48 @@
+# Angular Calculator
+
+> Lets build a calculator using angular and Tailwind CSS
+
+HTML
+```HTML
+<!-- Adding a dark green(teal) background to complete screen -->
+<!-- 'min-w-screen' will set div width to full screen width -->
+<!-- 'min-h-screen' it will set div hight to full screen hight -->
+<!-- 'bg-teal-600' used to add background colors -->
+<!-- 'flex' it will help 'justify-center' and 'items-center' to centerize content  -->
+<!-- 'px' will add padding in x axis, and 'py' will add padding on y axis -->
+<div class="
+    min-w-screen
+    min-h-screen
+    bg-teal-600
+    flex
+    items-center
+    justify-center
+    px-5
+    py-5
+    ">
+</div>
+
+HTML
+```HTML
+<div class=" min-w-screen min-h-screen bg-teal-600 flex items-center justify-center px-5 py-5
+    ">
+    <!-- 'rounded-xl' will set this div border to round -->
+    <!-- 'mx-auto' will set margin to auto -->
+    <!-- 'shadow-xl' this will add shaddow outside div -->
+    <!-- 'text-gray-800' this will add text color to gray -->
+    <!-- 'relative' class is to make div-display relative -->
+    <!-- Overflow 404 (i dont know video me btaya hi nahi) -->
+    <div class="w-full
+        mx-auto
+        rounded-xl
+        shadow-xl
+        text-gray-800
+        relative
+        overflow-hidden
+        " style="max-width: 350px;">
+        <h1 class="text-center py-10 text-3xl font-bold">Angular Calculator</h1>
+
+    </div>
+
+</div>
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ɵNgNoValidate } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,100 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculator';
+
+  calValue: number = 0;
+  funcT: any = 'No Function';
+
+  calNumber: string = 'noValue';
+
+  firstNumber: number = 0;
+  secondNumber: number = 0;
+
+  onClickValue(value: string, type: any) {
+    if (type == 'number') {
+      this.onNumberClick(value);
+    }else if(type == "function"){
+      this.onFunctionClick(value);
+    }
+
+  }
+
+  onNumberClick(value: string) {
+    if (this.calNumber != 'noValue') {
+      this.calNumber = this.calNumber + value;
+
+    } else {
+      this.calNumber = value;
+    }
+    this.calValue = parseFloat(this.calNumber);
+  }
+
+  onFunctionClick(val :string){
+    if(this.funcT == 'c'){
+      this.clearAll();
+    }
+    if(this.funcT == 'No Function'){
+      this.firstNumber = this.calValue;
+      this.calValue = 0;
+      this.calNumber = 'noValue';
+      this.funcT = val;
+    }else if(this.funcT != 'No Function'){
+      this.secondNumber = this.calValue;
+
+      this.valueCalculate(val);
+    }
+  }
+
+  valueCalculate(val: string){
+    if(this.funcT == '+'){
+      const result = this.firstNumber + this.secondNumber;
+      this.totalAssignValues(result, val);
+      if(this.funcT == '='){ this.onEqualPress() }
+    }
+    if(this.funcT == '-'){
+      const result = this.firstNumber - this.secondNumber;
+      this.totalAssignValues(result, val);
+      if(this.funcT == '='){ this.onEqualPress() }
+    }
+    if(this.funcT == '*'){
+      const result = this.firstNumber * this.secondNumber;
+      this.totalAssignValues(result, val);
+      if(this.funcT == '='){ this.onEqualPress() }
+    }
+    if(this.funcT == '%'){
+      const result = this.firstNumber % this.secondNumber;
+      this.totalAssignValues(result, val);
+      if(this.funcT == '='){ this.onEqualPress() }
+    }
+    if(this.funcT == '/'){
+      const result = this.firstNumber / this.secondNumber;
+      this.totalAssignValues(result, val);
+      if(this.funcT == '='){ this.onEqualPress() }
+    }
+  }
+
+  totalAssignValues(result: number, val: string){
+    this.calValue = result;
+      this.firstNumber = result;
+      this.secondNumber = 0;
+      this.calNumber = 'noValue';
+      this.funcT = val;
+  }
+  onEqualPress(){
+    this.firstNumber = 0;
+    this.secondNumber = 0;
+    this.funcT = 'No Function';
+    this.calNumber = 'noValue';
+  }
+  clearAll(){
+    this.firstNumber = 0;
+    this.secondNumber = 0;
+    this.funcT = 'No Function';
+    this.calNumber = 'noValue';
+    this.calValue = 0;
+
+  }
+
+  
+  
 }
